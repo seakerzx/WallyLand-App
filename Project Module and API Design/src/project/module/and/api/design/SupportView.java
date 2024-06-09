ad893ea40796a22bbdfad8083f5c29cb3091b375
amayes61 <ajm8473@psu.edu>
2024-06-09 13:24:46 -0400

    initial commit

---
+++ Project Module and API Design/src/project/module/and/api/design/SupportView.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project.module.and.api.design;
+
+/**
+ *
+ * @author lloydadkins
+ */
+public class SupportView {
+    public SupportView(int caseNumber, boolean closed){
+        
+    }
+}
